a266e43441ac2c63520ae535f576ab4975fe26b0
Patrick <g00409592@atu.ie>
2022-11-29 14:28:34 +0000

    Customer and Person Class

---
+++ src/main/java/ie/atu/Customer.java
@@ -0,0 +1,37 @@
+package ie.atu;
+
+public class Customer extends Person{
+    private String customerNumber;
+    private boolean mailingList;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String number, String customerNumber, boolean mailingList) {
+        super(name, address, number);
+        this.customerNumber = customerNumber;
+        this.mailingList = mailingList;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public boolean isMailingList() {
+        return mailingList;
+    }
+
+    public void setMailingList(boolean mailingList) {
+        this.mailingList = mailingList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "customerNumber='" + customerNumber + '\'' +
+                ", mailingList=\'" + mailingList + '\'';
+    }
+}
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,47 @@
 package ie.atu;
 
 public class Person {
+    private String name;
+    private String address;
+    private String number;
+
+    public Person() {
+    }
+
+    public Person(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", number='" + number + '\'' + ", ";
+    }
 }
